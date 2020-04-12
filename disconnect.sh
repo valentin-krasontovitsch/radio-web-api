d9c83e27a17704126632f68d21a30ed6b58de2c2
Valentin Krasontovitsch <v.krasontov@gmail.com>
2020-04-12 14:50:37 +0200

    Fix: Correct disconnect script to wait for succesful disconnect

--- disconnect.sh
+++ disconnect.sh
@@ -6,8 +6,24 @@ if [ -z "$SPEAKER_ADDRESS" ]; then
 fi
 
 export SPEAKER_ADDRESS
-connected=$(connected.sh)
 
+connected=$(connected.sh)
 if [ "$connected" = "no" ]; then echo 'not connected!' && exit 0; fi
 
 echo "disconnect $SPEAKER_ADDRESS" | bluetoothctl &>/dev/null
+
+trial=1
+max_trials=5
+while true; do
+  connected=$(connected.sh)
+  if [ "$connected" = "no" ]; then
+    exit 0
+  else
+    if [ $trial -eq $max_trials ]; then
+      errecho "Failed to disconnect?!"
+      exit 1
+    fi
+  fi
+  let "trial += 1"
+  sleep 1
+done
